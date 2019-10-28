aab68946ef9065914ed18f1bb6cae69800075f10
siim <siimpuustusmaa@gmail.com>
2019-10-28 08:56:54 +0200

    one piece hadwrong side

--- src/PuzzlePiece.ts
+++ src/PuzzlePiece.ts
@@ -47,7 +47,7 @@ class PuzzlePiece {
 const PIECES = [
     new PuzzlePiece(1, [PieceSide.FLAG_END, PieceSide.FISH_END, PieceSide.FLOWER_END, PieceSide.BIRD_END]),
     new PuzzlePiece(2, [PieceSide.BIRD_START, PieceSide.FLOWER_START, PieceSide.FLAG_START, PieceSide.FLOWER_END]),
-    new PuzzlePiece(3, [PieceSide.FISH_END, PieceSide.BIRD_START, PieceSide.BIRD_END, PieceSide.FLAG_END]),
+    new PuzzlePiece(3, [PieceSide.FISH_END, PieceSide.BIRD_START, PieceSide.BIRD_END, PieceSide.FLAG_START]),
     new PuzzlePiece(4, [PieceSide.FLOWER_START, PieceSide.FISH_END, PieceSide.FISH_START, PieceSide.FLAG_END]),
     new PuzzlePiece(5, [PieceSide.BIRD_START, PieceSide.FLAG_START, PieceSide.FISH_START, PieceSide.FLOWER_START]),
     new PuzzlePiece(6, [PieceSide.FISH_END, PieceSide.FLAG_END, PieceSide.BIRD_START, PieceSide.FLOWER_END]),
